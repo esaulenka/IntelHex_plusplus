77f6c8d2f2170787f5029e96ff2a59a203789323
Alexey Esaulenko <esaulenka@gmail.com>
2020-09-29 00:02:13 +0300

    improve mingw fix

--- tests/TestsMain.cpp
+++ tests/TestsMain.cpp
@@ -1,12 +1,9 @@
-﻿
+﻿// strange bug "undefined reference to `WinMain' " in MinGW
+#ifdef __MINGW32__
+	#define DO_NOT_USE_WMAIN
+#endif
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 
-// strange bug "undefined reference to `WinMain' " in MinGW
-#ifdef __MINGW32__
-// Standard C/C++ main entry point
-int main (int argc, char * argv[]) {
-	return Catch::Session().run( argc, argv );
-}
-#endif
